caacf7d07817f453dca59786126f575101dd65aa
tikisailor <40813246+tikisailor@users.noreply.github.com>
2024-06-16 11:09:42 +0800

    adjust validation rules

--- libs/dtos/src/app.dtos.ts
+++ libs/dtos/src/app.dtos.ts
@@ -15,16 +15,13 @@ export class IncomingMessageNotificationDto {
   'stripped-html': string; // The HTML version of the message, without the quoted parts
   attachments: string; // The string that contains a JSON list of metadata objects, one for each attachment.
   @IsUrl({
-    protocols: process.env.NODE_ENV !== 'prod' ? ['http', 'https'] : ['https'],
+    protocols: process.env.NODE_ENV === 'prod' ? ['https'] : ['http', 'https'],
     allow_underscores: true,
+    require_protocol: true,
     host_whitelist:
-      process.env.NODE_ENV !== 'prod'
-        ? ['example.com']
-        : [
-            'mailgun.net',
-            'storage-us-west1.api.mailgun.net',
-            'api.mailgun.net',
-          ],
+      process.env.NODE_ENV === 'prod'
+        ? ['mailgun.net', 'storage-us-east4.api.mailgun.net', 'api.mailgun.net']
+        : ['example.com', 'storage-us-east4.api.mailgun.net'],
   })
   'message-url': string; // A URL that you can use to get and/or delete the message. Only present in the payload posted to the notification URL
   timestamp: number; // The number of seconds passed since January 1, 1970 (See Securing Webhooks)
--- scripts/utils/sample-request.sh
+++ scripts/utils/sample-request.sh
@@ -20,7 +20,7 @@ curl -X POST https://plated-mesh-423803-e0.uc.r.appspot.com/inbox -H "Content-Ty
 
 curl -X POST http://localhost:3000/inbox -H "Content-Type: application/json" -d '{
   "domain": "example.com",
-  "recipient": "recipient@example.com",
+  "recipient": "chat@mg.gptmail.me",
   "sender": "example@protonmail.com",
   "from": "Alice <alice@example.com>",
   "subject": "Test Subject",
@@ -30,10 +30,10 @@ curl -X POST http://localhost:3000/inbox -H "Content-Type: application/json" -d 
   "body-html": "<html><body>This is the HTML version of the email.</body></html>",
   "stripped-html": "<html><body>This is the stripped HTML version of the email without quotes.</body></html>",
   "attachments": "[{\"filename\":\"attachment.txt\",\"content-type\":\"text/plain\",\"size\":12345}]",
-  "message-url": "http://example.com/message/12345",
+  "message-url": "https://storage-us-east4.api.mailgun.net/v3/domains/mg.gptmail.me/messages/BAABAAFVO9zxG_LACg9CX765GqnnSrOgZ",
   "timestamp": 1622470421,
   "token": "abcdefghijklmnopqrstuvwxyz1234567890ABCDE",
-  "signature": "d41d8cd98f00b204e9800998ecf8427e",
+  "signature": "1c7af48c48e0a0db53cf6ffa1f64b256bce1897c48c261afb61943820100c8a9",
   "message-headers": "{\"Received\":[\"from mail.example.com (mail.example.com [192.0.2.1]) by smtp.example.com with ESMTP id abc123\"],\"Subject\":\"Test Subject\"}",
   "Content-id-map": "{}"
 }'
